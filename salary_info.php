855cb6be0de214a5ae8b99fbf44082c2fdd91e89
hlanier14 <hlanier90@gmail.com>
2021-12-09 23:22:06 +0000

    updated php

--- salary_info.php
+++ salary_info.php
@@ -2,15 +2,14 @@
 <html>
 <body>
 
+<!-- connect to database -->
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "DBFall2021*";
 $db = "salary";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -18,31 +17,55 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 ?>
 
-<table border="1" align="center">
-<tr>
-  <td>company</td>
-</tr>
-<form action="" method="get">
-<input type="text" name="title" id="title"></input>
-</form>
 
+
+
+<!-- get user input title to find company with highest salary -->
+<br>>
+<p>Find out which company has the highest salary for the title: </p>
+<form action="" method="get">
+	<input type="text" name="company_max_salary_title" id="company_max_salary_title"></input>
+</form>
+<!-- use user input title in query and output result -->
 <?php
-$title = htmlentities($_GET['title']);
+$company_max_salary_title = htmlentities($_GET['company_max_salary_title']);
 $query = mysqli_query($conn, "Select company
                               From Salary
-                              Where title = \"$title\"
+                              Where title = \"$company_max_salary_title\"
                               And total_yearly_compensation = 
-                              (select max(total_yearly_compensation) from Salary where title = \"$title\");")
+                              (select max(total_yearly_compensation) from Salary where title = \"$company_max_salary_title\");")
 or die (mysqli_error($conn));
-
+// Output row of query, should only be one row for this question
 while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['company']}</td>
-   </tr>";
+  echo "<p>{$row['company']}</p>";
 }
-
 ?>
+
+
+
+<!-- get user input title to find the average entry level salary -->
+<br>
+<p>Find out the average entry level salary for the title: </p>
+<form action="" method="get">
+        <input type="text" name="avg_entry_salary_title" id="avg_entry_salary_title"></input>
+</form>
+<!-- use user input title for query -->
+<?php
+$avg_entry_salary_title = htmlentities($_GET['avg_entry_salary_title']);
+$query = mysqli_query($conn, "Select avg(total_yearly_compensation) as avg
+		   	      From Salary
+			      Where title = \"$avg_entry_salary_title\"
+			      And years_of_experience = 0;")
+			      or die (mysqli_error($conn));
+while ($row = mysqli_fetch_array($query)) {
+   echo "<p>{$row['avg']}</p>";
+}
+?>
+
+
+
+
+
 
 </table>
 </body>
