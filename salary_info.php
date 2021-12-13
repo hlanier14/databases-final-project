2fe072f5589f49637c058619826770c08e0daeb1
hlanier14 <hlanier90@gmail.com>
2021-12-13 18:37:39 +0000

    title dropdown

--- salary_info.php
+++ salary_info.php
@@ -23,21 +23,29 @@ echo "Connected successfully";
 <!-- get user input title to find company with highest salary -->
 <br>>
 <p>Find out which company has the highest salary for the title: </p>
-<form action="" method="get">
-	<input type="text" name="company_max_salary_title" id="company_max_salary_title"></input>
-</form>
 <!-- use user input title in query and output result -->
 <?php
-$company_max_salary_title = htmlentities($_GET['company_max_salary_title']);
-$query = mysqli_query($conn, "Select company
-                              From Salary
-                              Where title = \"$company_max_salary_title\"
-                              And total_yearly_compensation = 
-                              (select max(total_yearly_compensation) from Salary where title = \"$company_max_salary_title\");")
+$title_query = mysqli_query($conn, "Select distinct title from Salary;") or die (mysqli_error($conn));
+echo "<form action='' method='POST'>";
+echo "<select name='company_max_salary_title' onchange='this.form.submit()'>";
+while ($row = mysqli_fetch_array($title_query)) {
+   echo "<option value='" . $row['title'] . "'>" . $row['title'] . "</option>";
+}
+echo "</select>";
+echo "</form>";
+?>
+
+<?php
+$company_max_salary_title = htmlentities($_POST['company_max_salary_title']);
+$sal_query = mysqli_query($conn, "Select company, total_yearly_compensation
+                                 From Salary
+                                 Where title = \"$company_max_salary_title\"
+                                 And total_yearly_compensation = 
+                                 (select max(total_yearly_compensation) from Salary where title = \"$company_max_salary_title\");")
 or die (mysqli_error($conn));
 // Output row of query, should only be one row for this question
-while ($row = mysqli_fetch_array($query)) {
-  echo "<p>{$row['company']}</p>";
+while ($row = mysqli_fetch_array($sal_query)) {
+  echo "<p>{$row['company']} pays a {$company_max_salary_title} employee a total compensation of $ {$row['total_yearly_compensation']}</p>";
 }
 ?>
 
