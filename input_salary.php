dfc8e59e78176362eeff989751443df8cb3e9ed1
Harrison Lanier <60249654+hlanier14@users.noreply.github.com>
2021-12-14 11:26:39 -0600

    Update input_salary.php

--- input_salary.php
+++ input_salary.php
@@ -1,7 +1,7 @@
 <html>
 <body> 
 
-
+<!-- css formatting -->
 <head>
 <style>
 	form  {display: table;}
@@ -37,8 +37,12 @@ echo "Connected successfully";
 <!-- Need company, location, title, total_yearly_compensation, years_of_experience, years_at_company, stock_grant_value, bonus, highest lvl of education -->
 <!-- Assign id of # rows + 1, datetime of submited time, city_id of associated location, bachelors masters and doctorate from highest lvl of education -->
 
+<!-- send to thank_you.php page after submission -->
 <form action="thank_you.php" method="POST">
+
+<!-- inputs -->
 <p><label for="company"> Company: </label> <input type="text" id="company" name="company"><br></p>
+<!-- query all cities for drop down -->
 <p><label for="city">City: </label>
 <?php
 $city_query = mysqli_query($conn, "Select distinct location from Location;") or die (mysqli_error($conn));
@@ -49,14 +53,14 @@ while ($row = mysqli_fetch_array($city_query)) {
    echo "<option value='" . $row['location'] . "'>" . $row['location'] . "</option>";
 }
 echo "</select>";
-?>
-</p>
+?></p>
 <p><label for="title">Title: </label> <input type="text" id="title" name="title"><br></p>
 <p><label for="total_yearly_compensation">Total Yearly Compensation: </label><input type="number" id="total_yearly_compensation" name="total_yearly_compensation"><br></p>
 <p><label for="stock_grant_value">Stock Grant: </label><input type="number" id="stock_grant_value" name="stock_grant_value"><br></p>
 <p><label for="bonus">Bonus: </label><input type="number" id="bonus" name="bonus"><br></p>
 <p><label for="years_of_experience">Years of Experience: </label><input type="number" id="years_of_experience" name="years_of_experience"><br></p>
 <p><label for="years_at_company">Years at Company: </label><input type="number" id="years_at_company" name="years_at_company"><br></p>
+<!-- use drop down for highest education and calculate input attributes from selected-->
 <p><label for="education"> Highest Level of Education: </label><select name="education" id="education">
 	<option selected='selected' value='select'>Select</option>
 	<option value="high_school">High School</option>
